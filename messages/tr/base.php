a01e7eeca697800248e8709aa01ed7f40c23eeb2
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-12-30 07:27:25 +0900

    Fix: Formatting

--- messages/tr/base.php
+++ messages/tr/base.php
@@ -1,9 +1,8 @@
 <?php
-
-	return [
-		'<strong>Steam</strong>' => '<strong>Steam</strong>',
-		'Steam Settings' => 'Steam Ayarlar',
-		'Steam Widget URL:' => 'Steam Widget URL:',
-		'<strong>Steam</strong> module configuration' => '<strong>Steam</strong> modül yapılandırması',
-		'Save' => 'Kaydet',
-	];
+return [
+    '<strong>Steam</strong>' => '<strong>Steam</strong>',
+    'Steam Settings' => 'Steam Ayarlar',
+    'Steam Widget URL:' => 'Steam Widget URL:',
+    '<strong>Steam</strong> module configuration' => '<strong>Steam</strong> modül yapılandırması',
+    'Save' => 'Kaydet',
+];
