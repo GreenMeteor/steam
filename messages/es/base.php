ae1f54981b5858d527d4cf95a965a58d6b90f5f9
xdCL <67495563+xdCL@users.noreply.github.com>
2020-08-20 23:36:11 -0400

    Update base.php

--- messages/es/base.php
+++ messages/es/base.php
@@ -1,8 +1,8 @@
 <?php
 	return [
-		'<strong>Steam</strong>' => '',
-		'Steam Settings' => '',
-		'Steam Widget URL:' => '',
-		'<strong>Steam</strong> module configuration' => '',
-		'Save' => '',
+		'<strong>Steam</strong>' => '<strong>Steam</strong>',
+		'Steam Settings' => 'Ajustes de Steam',
+		'Steam Widget URL:' => 'URL del Steam Widget',
+		'<strong>Steam</strong> module configuration' => 'Configuración del módulo <strong>Steam</strong>',
+		'Save' => 'Guardar',
 	];
